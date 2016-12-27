3198629f62c066996d8b62123002ab87341a4284
Raymond Chua <ray.r.chua@gmail.com>
2016-12-27 19:06:51 +0100

    change the dataset to the brain dataset

---
+++ linear_regression/cmake-build-debug/CMakeFiles/linear_regression.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linear_regression.dir/main.cpp.o"
+  "linear_regression.pdb"
+  "linear_regression"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linear_regression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
